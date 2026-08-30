fd3bd67b74ac4d2f71d8d072136c5380a8742ef3
akemi <sergeytolkahev360@gmail.com>
2026-08-30 22:02:33 +0300

    made logic

--- js/app.js
+++ js/app.js
@@ -1,2 +1,24 @@
 "use strict";
-console.log("Hello World");
+const items = document.querySelectorAll(".promo__timer-item > h4");
+let countdownDate = new Date(2027, 11, 18, 10, 0, 0).getTime();
+function getCountdownTime() {
+    const now = new Date().getTime();
+    const distance = countdownDate - now;
+    const oneDay = 24 * 60 * 60 * 1000;
+    const oneHour = 60 * 60 * 1000;
+    const oneMinute = 60 * 1000;
+    let days = Math.floor(distance / oneDay);
+    let hours = Math.floor((distance % oneDay) / oneHour);
+    let minutes = Math.floor((distance % oneHour) / oneMinute);
+    let seconds = Math.floor((distance % oneMinute) / 1000);
+    const values = [days, hours, minutes, seconds];
+    console.log(values);
+    items.forEach((item, index) => {
+        const value = values[index];
+        item.textContent = value !== undefined ? String(value) : "";
+    });
+}
+getCountdownTime();
+setInterval(() => {
+    getCountdownTime();
+}, 1000);
--- ts/app.ts
+++ ts/app.ts
@@ -1,1 +1,31 @@
-console.log("Hello World");
+const items = document.querySelectorAll<HTMLParagraphElement>(
+  ".promo__timer-item > h4",
+);
+
+let countdownDate: number = new Date(2027, 11, 18, 10, 0, 0).getTime();
+
+function getCountdownTime() {
+  const now: number = new Date().getTime();
+
+  const distance: number = countdownDate - now;
+
+  const oneDay: number = 24 * 60 * 60 * 1000;
+  const oneHour: number = 60 * 60 * 1000;
+  const oneMinute: number = 60 * 1000;
+
+  let days: number = Math.floor(distance / oneDay);
+  let hours: number = Math.floor((distance % oneDay) / oneHour);
+  let minutes: number = Math.floor((distance % oneHour) / oneMinute);
+  let seconds: number = Math.floor((distance % oneMinute) / 1000);
+
+  const values: number[] = [days, hours, minutes, seconds];
+  console.log(values);
+  items.forEach((item, index) => {
+    const value = values[index];
+    item.textContent = value !== undefined ? String(value) : "";
+  });
+}
+getCountdownTime();
+setInterval(() => {
+  getCountdownTime();
+}, 1000);
